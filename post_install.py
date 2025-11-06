0b6ab19e4bf2417b487930b77099efd1c65ff592
Samir Kipper <samir.kipper@dlr.de>
2025-11-06 13:44:41 +0100

    cleaned up setup

--- post_install.py
+++
@@ -1,9 +0,0 @@
-import nltk
-
-def download_nltk_data():
-    nltk.download('wordnet')
-    nltk.download('omw-1.4')
-    nltk.download('punkt')
-
-if __name__ == '__main__':
-    download_nltk_data()
--- setup.sh
+++ setup.sh
@@ -3,5 +3,3 @@ python3 -m venv embed
 source embed/bin/activate
 pip install --upgrade pip
 pip install -r requirements.txt
-python post_install.py
-docker compose up -d
